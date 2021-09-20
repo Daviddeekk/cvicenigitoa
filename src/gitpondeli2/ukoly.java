7b9766d46cbaad83879df0032c408bd0bf989e3a
Daviddeekk <davdan1@seznam.cz>
2021-09-20 11:57:19 +0200

    přidaná třída úkoly

---
+++ src/gitpondeli2/ukoly.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitpondeli2;
+
+/**
+ *
+ * @author Danek_David
+ */
+public class ukoly {
+    private String nazev;
+}
